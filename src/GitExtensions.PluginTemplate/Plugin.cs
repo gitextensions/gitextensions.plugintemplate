5aed8c441967488b7e6c5193cf3bb8b6e71e1dbc
Marek Fišera <mara@neptuo.com>
2019-06-03 17:49:31 +0200

    Initial version from maraf/GitExtensions.EmptyPlugin.

---
+++ src/GitExtensions.PluginTemplate/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using GitExtensions.PluginTemplate.Properties;
+using GitUIPluginInterfaces;
+using ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitExtensions.PluginTemplate
+{
+    /// <summary>
+    /// An empty Git Extensions plugin implementation.
+    /// </summary>
+    [Export(typeof(IGitPlugin))]
+    public class Plugin : GitPluginBase
+    {
+        public Plugin()
+        {
+            Name = "PluginTemplate";
+            Description = "Plugin Template";
+            Icon = Resources.Icon;
+        }
+
+        public override bool Execute(GitUIEventArgs e)
+        {
+            MessageBox.Show(e.OwnerForm, "Hello from the Plugin Template.", "Git Extensions");
+            return true;
+        }
+    }
+}
